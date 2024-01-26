14a9d54d12dfcecc391b959cc4cc24d74064c87f
chaseoqueso <chaseoqueso@gmail.com>
2024-01-26 13:03:56 -0800

    Networked scene management started

---
+++ Assets/Scripts/SceneTransitionHandler.cs
@@ -0,0 +1,123 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Unity.Netcode;
+
+public class SceneTransitionHandler : NetworkBehaviour
+{
+    static public SceneTransitionHandler Instance { get; internal set; }
+
+    [HideInInspector]
+    public delegate void ClientLoadedSceneDelegateHandler(ulong clientId);
+    [HideInInspector]
+    public event ClientLoadedSceneDelegateHandler OnClientLoadedScene;
+
+    [HideInInspector]
+    public delegate void SceneStateChangedDelegateHandler(SceneStates newState);
+    [HideInInspector]
+    public event SceneStateChangedDelegateHandler OnSceneStateChanged;
+
+    private int numberOfClientsLoaded;
+    
+    /// <summary>
+    /// Example scene states
+    /// </summary>
+    public enum SceneStates
+    {
+        MainMenu,
+        Lobby,
+        Ingame
+    }
+
+    private SceneStates sceneState;
+
+    /// <summary>
+    /// Awake
+    /// If another version exists, destroy it and use the current version
+    /// Set our scene state to INIT
+    /// </summary>
+    private void Awake()
+    {
+        if(Instance != this && Instance != null)
+        {
+            Destroy(Instance.gameObject);
+        }
+        Instance = this;
+        SetSceneState(SceneStates.MainMenu);
+        DontDestroyOnLoad(this);
+    }
+
+    /// <summary>
+    /// SetSceneState
+    /// Sets the current scene state to help with transitioning.
+    /// </summary>
+    /// <param name="sceneState"></param>
+    public void SetSceneState(SceneStates sceneState)
+    {
+        this.sceneState = sceneState;
+        OnSceneStateChanged?.Invoke(this.sceneState);
+    }
+
+    /// <summary>
+    /// GetCurrentSceneState
+    /// Returns the current scene state
+    /// </summary>
+    /// <returns>current scene state</returns>
+    public SceneStates GetCurrentSceneState()
+    {
+        return sceneState;
+    }
+
+    /// <summary>
+    /// Registers callbacks to the NetworkSceneManager. This should be called when starting the server
+    /// </summary>
+    public void RegisterCallbacks()
+    {
+        NetworkManager.Singleton.SceneManager.OnLoadComplete += OnLoadComplete;
+        
+    }
+
+    /// <summary>
+    /// Switches to a new scene
+    /// </summary>
+    /// <param name="scenename"></param>
+    public void SwitchScene(string scenename)
+    {
+        if(NetworkManager.Singleton.IsListening)
+        {
+            numberOfClientsLoaded = 0;
+            NetworkManager.Singleton.SceneManager.LoadScene(scenename, LoadSceneMode.Single);
+        }
+        else
+        {
+            SceneManager.LoadSceneAsync(scenename);
+        }
+    }
+
+    private void OnLoadComplete(ulong clientId, string sceneName, LoadSceneMode loadSceneMode)
+    {
+        numberOfClientsLoaded += 1;
+        OnClientLoadedScene?.Invoke(clientId);
+    }
+
+    public bool AllClientsAreLoaded()
+    {
+        return numberOfClientsLoaded == NetworkManager.Singleton.ConnectedClients.Count;
+    }
+
+    /// <summary>
+    /// ExitAndLoadStartMenu
+    /// This should be invoked upon a user exiting a multiplayer game session.
+    /// </summary>
+    public void ExitAndLoadStartMenu()
+    {
+        if (NetworkManager.Singleton != null && NetworkManager.Singleton.SceneManager != null)
+        {
+            NetworkManager.Singleton.SceneManager.OnLoadComplete -= OnLoadComplete;
+        }
+        
+        OnClientLoadedScene = null;
+        SetSceneState(SceneStates.MainMenu);
+        SceneManager.LoadScene(GameManager.MAIN_MENU_SCENE);
+    }
+}
--- Assets/Scripts/UI Scripts/MainMenu.cs
+++ Assets/Scripts/UI Scripts/MainMenu.cs
@@ -1,25 +1,198 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using System.Text.RegularExpressions;
 
 public class MainMenu : MonoBehaviour
 {
+    public enum NetworkMode
+    {
+        Host,
+        Client
+    }
+
     public TMP_InputField IPInputField;
     public TMP_InputField portInputField;
+    public TMP_Text menuStatusText;
+    public Button startButton;
+    public Button backButton;
 
-    public void HostButton()
+    NetworkManager networkManager;
+    UnityTransport networkTransport;
+    
+    // This is needed to make the port field more convenient. GUILayout.TextField is very limited and we want to be able to clear the field entirely so we can't cache this as ushort.
+    string portString = "7777";
+    string connectAddress = "127.0.0.1";
+    NetworkMode networkMode;
+    TMP_Text startServerButtonText;
+
+    void Awake()
     {
+        // Only cache networking manager but not transport here because transport could change anytime.
+        networkManager = NetworkManager.Singleton;
+        startServerButtonText = startButton.GetComponentInChildren<TMP_Text>();
 
+        IPInputField.text = connectAddress;
+        portInputField.text = portString;
     }
-    public void TryJoinGame()
+    
+    void Start()
     {
-
+        networkTransport = (UnityTransport)networkManager.NetworkConfig.NetworkTransport;
+        
+        ShowStatusText(false);
+        
+        NetworkManager.Singleton.OnClientConnectedCallback += OnOnClientConnectedCallback;
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnOnClientDisconnectCallback;
     }
+
+    void OnOnClientConnectedCallback(ulong obj)
+    {
+        // Do Stuff
+    }
+
+    void OnOnClientDisconnectCallback(ulong clientId)
+    {
+        if (NetworkManager.Singleton.IsServer && clientId != NetworkManager.ServerClientId)
+        {
+            return;
+        }
+        // Do Stuff
+    }
+
+    bool SetConnectionData()
+    {
+        connectAddress = SanitizeInput(IPInputField.text);
+        portString = SanitizeInput(IPInputField.text);
+
+        if (connectAddress == "")
+        {
+            menuStatusText.text = "IP Address Invalid";
+            StopAllCoroutines();
+            StartCoroutine(ShowInvalidInputStatus());
+            return false;
+        }
+        
+        if (portString == "")
+        {
+            menuStatusText.text = "Port Invalid";
+            StopAllCoroutines();
+            StartCoroutine(ShowInvalidInputStatus());
+            return false;
+        }
+
+        if (ushort.TryParse(portString, out ushort port))
+        {
+            networkTransport.SetConnectionData(connectAddress, port);
+        }
+        else
+        {
+            networkTransport.SetConnectionData(connectAddress, 7777);
+        }
+        return true;
+    }
+
+    public void NetworkMenuHost()
+    {
+        networkMode = NetworkMode.Host;
+        startServerButtonText.text = "Host";
+    }
+
+    public void NetworkMenuClient()
+    {
+        networkMode = NetworkMode.Client;
+        startServerButtonText.text = "Join";
+    }
+
+    public void StartNetworkButton()
+    {
+        if(networkMode == NetworkMode.Host)
+        {
+            StartHost();
+        }
+        else
+        {
+            TryJoinGame();
+        }
+    }
+    
+    public void BackButton()
+    {
+        networkManager.Shutdown();
+    }
+
+    private void StartHost()
+    {
+        if (SetConnectionData() && NetworkManager.Singleton.StartHost())
+        {
+            SceneTransitionHandler.Instance.RegisterCallbacks();
+            SceneTransitionHandler.Instance.SwitchScene(GameManager.CHAR_SELECT_SCENE);
+        }
+    }
+
+    private void TryJoinGame()
+    {
+        if (SetConnectionData())
+        {
+            NetworkManager.Singleton.StartClient();
+            StopAllCoroutines();
+            StartCoroutine(ShowConnectingStatus());
+        }
+    }
+
     public void ExitGame()
     {
         Debug.Log ("Quit");
         Application.Quit();
+    }
+    
+    static string SanitizeInput(string dirtyString)
+    {
+        // sanitize the input for the ip address
+        return Regex.Replace(dirtyString, "[^0-9.]", "");
+    }
+    
+    void ShowStatusText(bool visible)
+    {
+        Color c = menuStatusText.color;
+        menuStatusText.color = visible ? new(c.r, c.g, c.b, 1) : new(c.r, c.g, c.b, 0);
+    }
+
+    IEnumerator ShowInvalidInputStatus()
+    {
+        ShowStatusText(true);
+
+        yield return new WaitForSeconds(3f);
+        
+        ShowStatusText(false);
+    }
+    
+    IEnumerator ShowConnectingStatus()
+    {
+        menuStatusText.text = "Attempting to Connect...";
+        ShowStatusText(true);
+        
+        startButton.interactable = false;
+        backButton.interactable = false;
+        
+        var unityTransport = networkManager.GetComponent<UnityTransport>();
+        var connectTimeoutMs = unityTransport.ConnectTimeoutMS;
+        var maxConnectAttempts = unityTransport.MaxConnectAttempts;
+
+        yield return new WaitForSeconds(connectTimeoutMs * maxConnectAttempts / 1000f);
+
+        // wait to verify connect status
+        yield return new WaitForSeconds(1f);
+        
+        menuStatusText.text = "Connection Attempt Failed";
+        startButton.interactable = true;
+        backButton.interactable = true;
+        
+        yield return new WaitForSeconds(3f);
+        
+        ShowStatusText(false);
     }
 }
