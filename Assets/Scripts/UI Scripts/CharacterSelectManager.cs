40e4f5b02f7e276b50b6c80030c15521dc3d03e4
portaljumperjen <portaljumperjen@gmail.com>
2024-01-26 01:47:20 -0800

    character select stuff

--- Assets/Scripts/UI Scripts/CharacterSelectManager.cs
+++ Assets/Scripts/UI Scripts/CharacterSelectManager.cs
@@ -8,82 +8,120 @@ public class CharacterSelectManager : MonoBehaviour
 {
     public bool playerIsReady {get; private set;}
 
-    [SerializeField] private GameObject nextArrowPanel;
-    [SerializeField] private GameObject previousArrowPanel;
+    #region Interactable Objects
+        [SerializeField] private GameObject nextArrowPanel;
+        [SerializeField] private GameObject previousArrowPanel;
 
-    [SerializeField] private Button readyButton;
-    [SerializeField] private TMP_Text readyText;
-    [SerializeField] private Button quitButton;
-    [SerializeField] private Button randomizeButton;
+        [SerializeField] private Button readyButton;
+        [SerializeField] private TMP_Text readyText;
+        [SerializeField] private Button quitButton;
+        [SerializeField] private Button randomizeButton;
+
+        [SerializeField] private Slider speedSlider;
+        [SerializeField] private Slider weightSlider;
+        [SerializeField] private Slider handlingSlider;
+        [SerializeField] private Slider chargeSlider;
+    #endregion
+
+    [SerializeField] private GameObject lobbyPanelBackground;
 
     [SerializeField] private GameObject playerPanelPrefab;
 
     void Start()
     {
         playerIsReady = false;
+
+        SetStats();
+
+        DisplayNewPlayer(); // TODO: Pass in this player's info
     }
 
-    public void RotateHead(bool selectNext)
-    {
-        
-    }
+    #region Model/Stat Stuff
+        public void RotateHead(bool selectNext)
+        {
+            // TODO
 
-    public void RotateTorso(bool selectNext)
-    {
-        
-    }
-
-    public void RotateLegs(bool selectNext)
-    {
-        
-    }
-
-    public void ReadyButton()
-    {
-        // If the player is cancelling Ready, return to interactable UI
-        ToggleInteractableUI(playerIsReady);
-        playerIsReady = !playerIsReady;
-
-        if(playerIsReady){
-            readyText.text = "CANCEL";
-        }
-        else{
-            readyText.text = "READY";
+            SetStats();
         }
 
-        // TODO: Alert server
-    }
+        public void RotateTorso(bool selectNext)
+        {
+            // TODO
 
-    private void ToggleInteractableUI(bool toggle)
-    {
-        nextArrowPanel.SetActive(toggle);
-        previousArrowPanel.SetActive(toggle);
-        quitButton.interactable = toggle;
-        randomizeButton.interactable = toggle;
-    }
+            SetStats();
+        }
 
-    public void RandomizeButton()
-    {
-        // TODO: Generate three random numbers and select a random thing from each category
-    }
+        public void RotateLegs(bool selectNext)
+        {
+            // TODO
 
-    public void QuitButton()
-    {
-        // TODO: Alert server you left the lobby - tell all the clients to remove this player - and return to main menu
-    }
+            SetStats();
+        }
 
-    public void DisplayNewPlayer(int playerNumber)
-    {
-        // TODO: Instantiate new playerPanelPrefab - need player name and a tiny camera window of their character or at least the head
-    }
+        private void SetStats()
+        {
+            // TODO: Display the stats of that character
 
-    public void RemovePlayer(int playerNumber)
-    {
-        // TODO: Delete that panel
-    }
+            // TEMP for visualization purposes
+            speedSlider.value = Random.Range(1,11);
+            weightSlider.value = Random.Range(1,11);
+            handlingSlider.value = Random.Range(1,11);
+            chargeSlider.value = Random.Range(1,11);
+        }
+    #endregion
 
-    public void PlayerIsReady(int playerNumber, bool isReady)
-    {
-        // TODO: Tell the UI to toggle that one Ready (or no longer Ready)
-    }
+    #region Interactables
+        public void ReadyButton()
+        {
+            // If the player is cancelling Ready, return to interactable UI
+            ToggleInteractableUI(playerIsReady);
+            playerIsReady = !playerIsReady;
+
+            if(playerIsReady){
+                readyText.text = "CANCEL";
+            }
+            else{
+                readyText.text = "READY";
+            }
+
+            // TODO: Alert server
+        }
+
+        private void ToggleInteractableUI(bool toggle)
+        {
+            nextArrowPanel.SetActive(toggle);
+            previousArrowPanel.SetActive(toggle);
+            quitButton.interactable = toggle;
+            randomizeButton.interactable = toggle;
+        }
+
+        public void RandomizeButton()
+        {
+            // TODO: Generate three random numbers and select a random thing from each category
+        }
+
+        public void QuitButton()
+        {
+            // TODO: Alert server you left the lobby - tell all the clients to remove this player - and return to main menu
+        }
+    #endregion
+
+    #region Lobby Stuff
+        public void DisplayNewPlayer() // TODO: pass in player number or Player or something
+        {
+            Instantiate(playerPanelPrefab, new Vector3(0,0,0), Quaternion.identity, lobbyPanelBackground.transform);
+
+            // TODO: Player name and a tiny camera window of their character or at least the head
+        }
+
+        public void RemovePlayer() // TODO: pass in player number or Player or something
+        {
+            // TODO: Delete that panel
+        }
+
+        public void PlayerIsReady(bool isReady) // TODO: pass in player number or Player or something
+        {
+            // TODO: Tell the UI to toggle that one's Ready overlay (or no longer Ready)
+        }
+    #endregion
 }
---
+++ Assets/Scripts/UI Scripts/ModelSelectCircle.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelSelectCircle : MonoBehaviour
+{
+    // public GameObject currentHead;
+    // public GameObject currentTorso;
+    // public GameObject currentLegs;
+
+    void Start()
+    {
+        
+    }
+}
