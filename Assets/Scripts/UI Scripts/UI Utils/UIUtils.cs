dab841f15de3f34df193ba627f76fd4a8288424d
Jen <portaljumperjen@gmail.com>
2024-01-27 16:22:38 -0800

    in game UI progress

--- Assets/Scripts/UI Scripts/UI Utils/UIUtils.cs
+++ Assets/Scripts/UI Scripts/UI Utils/UIUtils.cs
@@ -5,7 +5,10 @@ using UnityEngine.UI;
 
 public class UIUtils
 {
-    public const string MAIN_COLOR = "#";
+    public const string DARK_ORANGE_COLOR = "#7A2600";
+    public const string YELLOW_COLOR = "#FFE62B";
+    public const string BLACK_COLOR = "#000000";
+    public const string LIGHT_ORANGE_COLOR = "#F6AB33";
 
     public static void SetImageColorFromHex( Image img, string hexCode )
     {
